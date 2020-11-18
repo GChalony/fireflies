35486355812401d2b11f6d8452f902e774f483c1
adrienmntr <adrien.matthieu.monteiro@gmail.com>
2020-11-18 16:33:22 +0100

    First push

---
+++ source/swarm.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Swarms:
+
+    def __init__(self, height, width, number):
+        self.positions = np.array()
+        self.directions = np.array()
+        self.shine = np.array()
+        self.clock = np.array()
+
